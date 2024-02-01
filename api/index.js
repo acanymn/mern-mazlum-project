e42fb93b6b5d9bb21380e9cd3ddacfc62e6773d1
acanymn <https://github.com/acanymn>
2024-02-01 19:17:47 +0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import env from 'dotenv';
+
+env.config();
+
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("MongoDB is connected!");
+}).catch((error) => {
+    console.log(error);
+});
+
 const app = express();
 
 app.listen(3000, () => {
